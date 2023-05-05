4fbeb9ef571f72aa055f5cc15c5f73b5469ff867
Noviana Ramadhani <novianarahmadani3@gmail.com>
2023-05-05 14:35:34 +0800

    Menambah home

---
+++ home.php
@@ -0,0 +1,135 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+if(isset($_POST['add_to_cart'])){
+
+   $vila_name = $_POST['vila_name'];
+   $vila_price = $_POST['vila_price'];
+   $vila_image = $_POST['vila_image'];
+   $vila_quantity = $_POST['vila_quantity'];
+
+   $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$vila_name' AND user_id = '$user_id'") or die('query failed');
+
+   if(mysqli_num_rows($check_cart_numbers) > 0){
+      $message[] = 'already added to cart!';
+   }else{
+      mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$vila_name', '$vila_price', '$vila_quantity', '$vila_image')") or die('query failed');
+      $message[] = 'vila added to cart!';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style1.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<section class="home">
+
+   <div class="content">
+      <h3>Silahkan Reservasi Sekarang</h3>
+      <p>Enjoy the moment</p>
+      <a href="about.php" class="white-btn">discover more</a>
+   </div>
+
+</section>
+
+<section class="products">
+
+   <h1 class="title">latest products</h1>
+
+   <div class="box-container">
+
+      <?php  
+         $select_vila = mysqli_query($conn, "SELECT * FROM `vila` LIMIT 6") or die('query failed');
+         if(mysqli_num_rows($select_vila) > 0){
+            while($fetch_vila = mysqli_fetch_assoc($select_vila)){
+      ?>
+     <form action="" method="post" class="box">
+      <img class="image" src="uploaded_img/<?php echo $fetch_vila['image']; ?>" alt="">
+      <div class="name"><?php echo $fetch_vila['name']; ?></div>
+      <div class="price">$<?php echo $fetch_vila['price']; ?>/-</div>
+      <input type="number" min="1" name="vila_quantity" value="1" class="qty">
+      <input type="hidden" name="vila_name" value="<?php echo $fetch_vila['name']; ?>">
+      <input type="hidden" name="vila_price" value="<?php echo $fetch_vila['price']; ?>">
+      <input type="hidden" name="vila_image" value="<?php echo $fetch_vila['image']; ?>">
+      <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+     </form>
+      <?php
+         }
+      }else{
+         echo '<p class="empty">no vila added yet!</p>';
+      }
+      ?>
+   </div>
+
+   <div class="load-more" style="margin-top: 2rem; text-align:center">
+      <a href="shop.php" class="option-btn">load more</a>
+   </div>
+
+</section>
+
+<section class="about">
+
+   <div class="flex">
+
+      <div class="image">
+         <img src="images/fotoB.jpg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>about us</h3>
+         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit quos enim minima ipsa dicta officia corporis ratione saepe sed adipisci?</p>
+         <a href="about.php" class="btn">read more</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="home-contact">
+
+   <div class="content">
+      <h3>have any questions?</h3>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque cumque exercitationem repellendus, amet ullam voluptatibus?</p>
+      <a href="contact.php" class="white-btn">contact us</a>
+   </div>
+
+</section>
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
